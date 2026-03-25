7ff68e8728bb5ca4af1d480fa2d6d7e10cb3cf51
Fredy V51 <fredy.bazalar@via51.org>
2026-03-24 21:22:46 -0500

    ORDEN: Activar detección automática para pacha.via51.org

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import PantallaAlfa from './components/Via51/PantallaAlfa';
 import Pacha from './pages/Pacha'; 
 
 function App() {
+  // Detectamos si el usuario entró por pacha.via51.org o via51.org/pacha
+  const isPacha = window.location.hostname.includes('pacha') || 
+                  window.location.pathname.includes('pacha');
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PantallaAlfa />} />
-        <Route path="/pacha" element={<Pacha />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      {isPacha ? <Pacha /> : <PantallaAlfa />}
+    </div>
   );
 }
 
